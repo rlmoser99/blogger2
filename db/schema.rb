a3ff730c098109eff9c5fc5cf1f46eb66ab1a47a
Rachel Moser <moser.rachel@gmail.com>
2020-02-14 21:20:09 -0600

    sorcery authentication complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_193836) do
+ActiveRecord::Schema.define(version: 2020_02_15_021448) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2020_02_13_193836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_attachments_on_article_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
